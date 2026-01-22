45fe2d65de3f719af52c12fd2251cc5b351984ed
Harsh Mehta <harshmehta.srm2002@gmail.com>
2026-01-22 09:41:23 +0530

    vercel error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,13 @@ import WhyLoveUs from "@/components/Home/WhyLoveUs";
 import HowItWorks from "@/components/Home/HowItWorks";
 import MembershipCards from "@/components/Home/MembershipCards";
 import WhatYouCanWin from "@/components/Home/WhatYouCanWin";
-import Testimonials from "@/components/Home/Testimonials";
+
 import FAQs from "@/components/Home/FAQs";
 
 export default function Home() {
   return (
     <div>
       <ComingSoon />
-      
     </div>
   );
 }
